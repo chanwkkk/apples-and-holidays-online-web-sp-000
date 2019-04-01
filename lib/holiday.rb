a08788a4a2e8ab28ddc65394f142d4cff5e341cb
Yingqi Chen <chanwk18@hotmail.com>
2019-04-01 22:06:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,6 +78,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 holiday_hash.each do |season,holiday_details|
+  holiday_details.select do |holidays,supplies|
+    supplies.include?("BBQ")
+    return holidays
 
 end
 end
